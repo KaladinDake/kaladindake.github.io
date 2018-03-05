b1806dee9674b255c78c91dbd4d29e5bd07c8812
KaladinDake <37079437+KaladinDake@users.noreply.github.com>
2018-03-05 15:16:42 -0500

    changing from temp to weather

--- weather.js
+++ weather.js
@@ -10,14 +10,14 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
+    ext.get_weather = function(location, callback) {
       console.log("Getting current weather");
       // Make an AJAX call to the Open Weather Maps API
       $.ajax({
           url: 'http://api.openweathermap.org/data/2.5/weather?zip=03063&appid=27f22360fce3c09eb4b3df095c988f3d',
           dataType: 'jsonp',
           success: function( weather_data ) {
-            console.log("Got current weather");
+            console.log("Got current weather " + weather_data);
               // Got the data - parse it and return the temperature
               temperature = weather_data['weather'][0]['main'];
               callback(temperature);
@@ -30,11 +30,11 @@
         blocks: [
             // Block type, block name, function name
 
-            ['R', 'current temperature in city %s', 'get_temp', '03063']
+            ['R', 'current weather in zipcode %s', 'get_weather', '03063']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register("Kaladin's extensions", descriptor, ext);
+    ScratchExtensions.register("Kaladin's weather extensions", descriptor, ext);
 
 })({});
