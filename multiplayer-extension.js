0b7adef6d050cf6ea45f92c52c9f98fe7d0ca2ce
KaladinDake <37079437+KaladinDake@users.noreply.github.com>
2018-04-04 20:26:02 -0400

    first try at informing a server

---
+++ multiplayer-extension.js
@@ -0,0 +1,52 @@
+(function(ext) {
+    var enterWasPressed = false;
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.im_playing = function(username, callback) {
+      console.log("tell server i'm playing");
+      // Make an AJAX call to the Open Weather Maps API
+
+      try {
+        $.ajax({
+            url: 'http://google.com/i.m-playing',
+            dataType: 'jsonp',
+            success: function( player_data ) {
+              console.log(weather_data);
+                // Got the data - parse it and return the temperature
+                count = player_data['count'];
+                callback(count);
+            },
+            error: function(player_data) {
+              console.log(player_data);
+              player_data = "Error";
+              callback(player_data);
+            }
+          });
+        } catch(err) {
+          console.log(err);
+          player_data = "Error";
+          callback(player_data);
+        }
+  };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+
+            ['R', 'how meny peaple are playing? Username: %s', 'im_playing', 'daker13']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register("Kaladin's martiplayer extensions", descriptor, ext);
+
+})({});
